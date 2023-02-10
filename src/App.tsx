903f7e5512afb75f065740a2a322ea8d38b653ec
mmgamble <mmgamble@udel.edu>
2023-02-10 12:35:20 -0500

    Added Hello World

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ function App(): JSX.Element {
             </header>
             <p>
                 Edit <code>src/App.tsx</code> and save. This page will
-                automatically reload. Meghan Gamble
+                automatically reload. Meghan Gamble Hello World
             </p>
         </div>
     );
