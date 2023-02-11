99e8066a18e83ce729ec0c0d28d8628ac64ac6a7
mmgamble <mmgamble@udel.edu>
2023-02-11 11:29:07 -0500

    Completed Adding Images, Headers, etc

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,9 @@
 import React from "react";
+import { Container } from "react-bootstrap";
+import { Row } from "react-bootstrap";
+import { Col } from "react-bootstrap";
+import { Button } from "react-bootstrap";
+
 import "./App.css";
 
 function App(): JSX.Element {
@@ -9,8 +14,47 @@ function App(): JSX.Element {
             </header>
             <p>
                 Edit <code>src/App.tsx</code> and save. This page will
-                automatically reload. Meghan Gamble Hello World
+                automatically reload. Meghan Gamble
             </p>
+
+            <h1>This is super cool!</h1>
+
+            <img src={require("./meme.png")} alt="This is a meme" />
+
+            <div>
+                <ul>
+                    <li>I write the code </li>
+                    <li>I run the code</li>
+                    <li>I have a bug</li>
+                    <li>I have a feature</li>
+                </ul>
+            </div>
+
+            <Container>
+                <Row>
+                    <Col>
+                        {" "}
+                        <div
+                            className="col1"
+                            style={{ backgroundColor: "red" }}
+                        ></div>{" "}
+                        It is Saturday
+                    </Col>
+                    <Col>
+                        <div
+                            className="col2"
+                            style={{ backgroundColor: "red" }}
+                        ></div>
+                        It is Sunny Outside
+                    </Col>
+                </Row>
+            </Container>
+
+            <div>
+                <Button onClick={() => console.log("Hello World!")}>
+                    Log Hello World
+                </Button>
+            </div>
         </div>
     );
 }
