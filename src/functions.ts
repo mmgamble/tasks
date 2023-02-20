3075fd37bfc935097cfa0d1a31182d2cd5856ce5
mmgamble <mmgamble@udel.edu>
2023-02-20 10:41:49 -0500

    Completed Function Tests

--- src/functions.ts
+++ src/functions.ts
@@ -4,7 +4,7 @@
  *      C = (F - 32) * 5/9
  */
 export function fahrenheitToCelius(temperature: number): number {
-    return 0;
+    return (temperature - 32) * (5 / 9);
 }
 
 /**
@@ -12,7 +12,24 @@ export function fahrenheitToCelius(temperature: number): number {
  * if the number is greater than zero.
  */
 export function add3(first: number, second: number, third: number): number {
-    return 0;
+    let addfirst: number = first;
+    let addsecond: number = second;
+    let addthird: number = third;
+
+    if (first > 0) {
+    } else {
+        addfirst = 0;
+    }
+    if (second > 0) {
+    } else {
+        addsecond = 0;
+    }
+    if (third > 0) {
+    } else {
+        addthird = 0;
+    }
+
+    return addfirst + addsecond + addthird;
 }
 
 /**
@@ -20,7 +37,7 @@ export function add3(first: number, second: number, third: number): number {
  * mark added to the end.
  */
 export function shout(message: string): string {
-    return "";
+    return message.toUpperCase() + "!";
 }
 
 /**
@@ -28,7 +45,7 @@ export function shout(message: string): string {
  * mark. Do not use an `if` statement in solving this question.
  */
 export function isQuestion(message: string): boolean {
-    return true;
+    return message.endsWith("?");
 }
 
 /**
@@ -37,5 +54,13 @@ export function isQuestion(message: string): boolean {
  * upper or lower case), then return `false`. Otherwise, return `null`.
  */
 export function convertYesNo(word: string): boolean | null {
-    return true;
+    let wordlower: string = word.toLowerCase();
+
+    if (wordlower === "yes") {
+        return true;
+    } else if (wordlower === "no") {
+        return false;
+    } else {
+        return null;
+    }
 }
