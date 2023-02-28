106c1d0f89a5f886a46007b18349cc0d61fea733
mmgamble <mmgamble@udel.edu>
2023-02-28 10:09:13 -0500

    addOption function complete

--- src/objects.ts
+++ src/objects.ts
@@ -161,7 +161,11 @@ export function duplicateQuestion(id: number, oldQuestion: Question): Question {
  * Check out the subsection about "Nested Fields" for more information.
  */
 export function addOption(question: Question, newOption: string): Question {
-    return question;
+    const q1 = {
+        ...question,
+        options: [...question.options, newOption]
+    };
+    return q1;
 }
 
 /**
