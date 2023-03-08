94a3c38263a983ddcdce033c7870d2be2a8f6bde
mmgamble <mmgamble@udel.edu>
2023-03-08 11:00:51 -0500

    Fixed Merge Errors (Added code back) and finished cycleholiday

--- src/App.tsx
+++ src/App.tsx
@@ -18,6 +18,49 @@ function App(): JSX.Element {
             <header className="App-header">
                 UD CISC275 with React Hooks and TypeScript
             </header>
+            <p>
+                Edit <code>src/App.tsx</code> and save. This page will
+                automatically reload. Meghan Gamble
+            </p>
+
+            <h1>This is super cool!</h1>
+
+            <img src={require("./memeCopy.png")} alt="This is a meme" />
+
+            <div>
+                <ul>
+                    <li>I write the code </li>
+                    <li>I run the code</li>
+                    <li>I have a bug</li>
+                    <li>I have a feature</li>
+                </ul>
+            </div>
+
+            <Container>
+                <Row>
+                    <Col>
+                        {" "}
+                        <div
+                            className="col1"
+                            style={{ backgroundColor: "red" }}
+                        ></div>{" "}
+                        It is Saturday
+                    </Col>
+                    <Col>
+                        <div
+                            className="col2"
+                            style={{ backgroundColor: "red" }}
+                        ></div>
+                        It is Sunny Outside
+                    </Col>
+                </Row>
+            </Container>
+
+            <div>
+                <Button onClick={() => console.log("Hello World!")}>
+                    Log Hello World
+                </Button>
+            </div>
 
             <hr></hr>
             <Counter></Counter>
