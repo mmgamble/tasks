5ad666b616a78b76a15268202d1368d85c9d17cc
mmgamble <mmgamble@udel.edu>
2023-03-17 11:11:38 -0400

    Finished GiveAttempts

--- src/form-components/GiveAttempts.tsx
+++ src/form-components/GiveAttempts.tsx
@@ -1,9 +1,61 @@
 import React, { useState } from "react";
+import { Form } from "react-bootstrap";
+import { Button } from "react-bootstrap";
+import { isNumericLiteral } from "typescript";
 
 export function GiveAttempts(): JSX.Element {
+    const [attempts, setattempts] = useState<number>(3);
+    const [reqattempts, updateattempts] = useState<number>(0);
+
+    function updateAtt(event: React.ChangeEvent<HTMLInputElement>) {
+        if (event.target.value === "") {
+            console.log("Input is NaN");
+            //    } else if (!isNaN(Number(event.target.value))) {
+            //      const tonum = Number(event.target.value);
+            //    updateattempts(tonum);
+        } else {
+            const tonum = Number(event.target.value);
+            updateattempts(tonum);
+        }
+    }
+
+    function usedisabled(): boolean {
+        if (attempts === 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
     return (
         <div>
-            <h3>Give Attempts</h3>
+            <div>
+                <Form.Group controlId="addAttempts">
+                    <Form.Label>Requested Attempts:</Form.Label>
+                    <Form.Control
+                        type="number"
+                        value={reqattempts}
+                        onChange={updateAtt}
+                    />
+                </Form.Group>
+            </div>
+            <div>Current Attempts: {attempts}</div>
+            <div>
+                <h3>Give Attempts</h3>
+            </div>
+            <div>
+                <Button
+                    onClick={() => setattempts(attempts - 1)}
+                    disabled={usedisabled()}
+                >
+                    use{" "}
+                </Button>
+            </div>
+            <div>
+                <Button onClick={() => setattempts(attempts + reqattempts)}>
+                    gain{" "}
+                </Button>
+            </div>
         </div>
     );
 }
